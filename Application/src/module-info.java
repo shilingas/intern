ea6fcc9d32efbf361d5b39f2e81abf6fa16ffe25
shilingas <mariussilingas5@gmail.com>
2022-05-29 22:15:10 +0300

    first

---
+++ Application/src/module-info.java
@@ -0,0 +1,13 @@
+module Applic {
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires javafx.media;
+	requires javafx.base;
+	requires java.desktop;
+	requires java.sql;
+	requires javafx.fxml;
+	requires javafx.swing;
+	requires javafx.web;
+
+	opens application;
+}
